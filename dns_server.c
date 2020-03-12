d9385cbea68b7a71139ab9adab374685c3c6355e
Sriharsha <nsharsha1919@gmail.com>
2020-03-12 22:40:44 +0530

    commented dns_server.c

--- dns_server.c
+++ dns_server.c
@@ -7,7 +7,7 @@
 #define PORT 8080 
 #define SA struct sockaddr 
 
-int stoi(char *st){
+int stoi(char *st){          //function that converts a string to integer,used for port number received
     int x=0, n=strlen(st);
     for(int i=0;i<n;i++){
         x *= 10;
@@ -16,7 +16,7 @@ int stoi(char *st){
     return x;
 }
 
-int whatUgonnado(int sockfd){
+int whatUgonnado(int sockfd){ //It is called once connection of proxy_server is accepted by DNS server
 	char buffer[256];
 	int n; 
 	memset(buffer,0,sizeof(buffer));
@@ -24,10 +24,10 @@ int whatUgonnado(int sockfd){
 	printf("proxy server sent %s to server\n",buffer);
 	
 	FILE *fptr;
-	fptr = fopen("dns.txt","r");
+	fptr = fopen("dns.txt","r"); //dns.txt is our directory of address mapings 
 	if(fptr == NULL)
 	{
-		printf("error in opening file\n");
+		printf("error 404: File not found\n");
 		return 0;
 	}
 	else
@@ -36,13 +36,12 @@ int whatUgonnado(int sockfd){
 	}
 
 	char file_line[256];
-	
 	int hit = 0;
 	char err_msg[256]="1entry not found in the database\0";
 
 	while(EOF != fscanf(fptr,"%[^\n]\n",file_line)){
-		char sip[20]="0";
-		char dname[30]="0";
+		char ip_addr[20]="0";
+		char dom_name[30]="0";
 		char delimiter='\0';
 		int i,j;
 		for (i = 0; i < 18; i++)
@@ -50,40 +49,39 @@ int whatUgonnado(int sockfd){
 			if(file_line[i]==' ')
 				break;
 		}
-		strncat(sip,file_line,i);
-		strncat(sip,&delimiter,1);
+		strncat(ip_addr,file_line,i);
+		strncat(ip_addr,&delimiter,1);
 
 		for (j = i+1; j < 256; j++)
 		{
 			if (file_line[j]=='\n')
 				break;
 		}
-		strncat(dname,file_line+i+1,j-i-1);
-		strncat(dname,&delimiter,1);  			
+		strncat(dom_name,file_line+i+1,j-i-1);
+		strncat(dom_name,&delimiter,1);  			
 		
-		//ip f[0->i-1] size i
-		//dom f[i+1 -> j-1] size j-i-1
+		//ip address is of length i and is f[0->i-1]
+		//domain name is of size j-i-1 and is f[i+1 -> j-1] 
 
-		// requesting IP
+		// proxy_server requesting IP addresss
 		if (buffer[0]=='0')
 		{
 			if(strncmp(file_line+i+1,buffer+1,j-i-1)==0)
 			{
-				printf("Hit! %s\n", sip);
-				int bytes_sent = send(sockfd, sip,strlen(sip)*sizeof(char),0);	
-				//printf("bytes sent: %d\n", bytes_sent);
+				printf("Boom Hit! Ip requested is:  %s\n", ip_addr);
+				send(sockfd, ip_addr,strlen(ip_addr)*sizeof(char),0);
 				hit = 1;
 				break;
 			}
 			
 		}
-		// requesting dname
+		//proxy_server requesting domain_name
 		else if (buffer[0]=='1')
 		{
 			if(strncmp(file_line,buffer+1,i)==0)
 			{
-				printf("matching string= %s\tstrlen_dname= %d\n", dname, (int)strlen(dname));
-				send(sockfd, dname,strlen(dname)*sizeof(char),0);
+				printf("Boom Hit! Domain name requested is:  %s\n", dom_name);
+				send(sockfd, dom_name,strlen(dom_name)*sizeof(char),0);
 				hit = 1;
 				break;
 			}
@@ -91,24 +89,21 @@ int whatUgonnado(int sockfd){
 	}
 	if (!hit)
 	{
-		int bytes_sent = send(sockfd,err_msg,strlen(err_msg)*sizeof(char),0);
-		printf("%s\tbytes_sent=%d\n", err_msg, bytes_sent);
+		send(sockfd,err_msg,strlen(err_msg)*sizeof(char),0);
+		printf("%s\n", err_msg);
 	}
-	//fprintf(,"%s\n",file_line );
 
 	fclose(fptr);
 	close(sockfd);
-	// and send that buffer to client 
-	// write(sockfd, buffer, sizeof(buffer));
 	return 0;
 }
 
 int main(int argc, char* argv[]){
 
-	int sockfd, connfd, binded;
+	int sockfd, connfd;
     // socket create and verification 
 	sockfd= socket(AF_INET,SOCK_STREAM,0);
-
+	//creating a IPv4 socket for TCP returns Socet file descriptor
     if (sockfd < 0)
     {
     	printf("hold up!socket creation failed bruh:(\n");
@@ -119,7 +114,7 @@ int main(int argc, char* argv[]){
     	printf("yayy! socket creation successful!\n");
 	}
 	int port = stoi(argv[1]);
-	// Configure the socket's address.
+	// Configurinng the socket's address.
 	struct sockaddr_in address;
     bzero(&address,sizeof(address));
     address.sin_family = AF_INET;
@@ -127,17 +122,16 @@ int main(int argc, char* argv[]){
     address.sin_port=htons(port);
 
 	if(bind(sockfd, (SA*) &address, sizeof(address)) < 0) {
-        printf("hold up!socket binding failed bruh!\n"); 
+        printf("hold up!socket binding failed!\n"); 
         return 0; 
     } 
     else
 	{
         printf("I renounce socket binded\n");
 	}
-    //ready to listen
     if ((listen(sockfd,5)) < 0)
     {
-    	printf("Dumbass socket!\n");
+    	printf("Arrogant socket!\n");
     	return 0;
     }
     else
